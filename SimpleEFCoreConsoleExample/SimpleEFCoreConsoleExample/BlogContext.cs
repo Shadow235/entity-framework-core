69d0a94081bb08a498c4fce4f232588fb08278f8
Shadow <tekken235@gmail.com>
2023-02-10 17:54:46 +0100

    BlogContext + Sqlite + usage example

---
+++ SimpleEFCoreConsoleExample/SimpleEFCoreConsoleExample/BlogContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleEFCoreConsoleExample
+{
+	internal class BlogContext : DbContext
+	{
+		public DbSet<Post> Posts { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+			=> optionsBuilder.UseSqlite("Data Source=Blogs.db");
+	}
+}
--- SimpleEFCoreConsoleExample/SimpleEFCoreConsoleExample/Program.cs
+++ SimpleEFCoreConsoleExample/SimpleEFCoreConsoleExample/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SimpleEFCoreConsoleExample;
+
+using var db = new BlogContext();
+
+db.Posts.Add(new Post { Title = "Topic 1", Content = "" });
+
+db.SaveChanges();
+
+var topic1 = db.Posts.FirstOrDefault(p => p.Title == "Topic 1");
+
+Console.ReadKey();
