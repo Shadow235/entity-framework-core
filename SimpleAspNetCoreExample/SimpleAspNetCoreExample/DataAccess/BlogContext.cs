bef59e82031f8616a50e5090ada344f48a7946e0
Shadow <tekken235@gmail.com>
2023-02-11 15:53:13 +0100

    SimpleAspNetCoreExample

---
+++ SimpleAspNetCoreExample/SimpleAspNetCoreExample/DataAccess/BlogContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleAspNetCoreExample.Models;
+
+namespace SimpleAspNetCoreExample.DataAccess
+{
+	public class BlogContext : DbContext
+	{
+		public BlogContext(DbContextOptions<BlogContext> options) : base(options){ }
+
+		public DbSet<Post> Posts { get; set; }
+	}
+}
